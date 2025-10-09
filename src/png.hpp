8cd601749d9b23aec47566c1e3789962b7a1e035
teremich <michael@teremich.de>
2025-10-09 23:57:06 +0200

    removed commented out code

--- src/png.hpp
+++ src/png.hpp
@@ -1,10 +1,6 @@
 #pragma once
 #include <mypng.hpp>
 
-// typedef std::uint8_t byte_t;
-// typedef byte_t data_t[];
-// typedef std::uint32_t crc_t;
-
 struct [[gnu::packed]] Chunk{
     std::uint32_t length;
     enum : std::uint32_t {
@@ -29,23 +25,3 @@ struct PNG_datastream{
     byte_t signature[8];
     struct Chunk chunks[];
 };
-
-// struct PNG{
-//     std::size_t totalSize;
-//     PNG_datastream* data;
-//     struct PLTE{
-//         size_t numColors;
-//         struct Color{
-//             uint8_t r, g, b;
-//         } *colors;
-//         size_t numTransparencies;
-//         uint8_t* transparencies;
-//     } palette;
-// };
-
-// const PNG loadPNG(const char* filename);
-// std::uint32_t* loadPixels(const PNG& png);
-// PNG createPNG(std::uint32_t* pixels, std::uint32_t width, std::uint32_t height);
-// void writePNG(const PNG& png, const char* filename);
-// void unloadPixels(std::uint32_t* pixels);
-// void unloadPNG(PNG png);
